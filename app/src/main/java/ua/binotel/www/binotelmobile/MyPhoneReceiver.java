b43dfa2b723610c22ed8082e6c68fe2aa154db82
Pavel <pavlo111188@gmail.com>
2018-01-12 14:06:24 +0200

    added window top messages

--- app/src/main/java/ua/binotel/www/binotelmobile/MyPhoneReceiver.java
+++ app/src/main/java/ua/binotel/www/binotelmobile/MyPhoneReceiver.java
@@ -6,11 +6,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.PixelFormat;
 import android.preference.PreferenceManager;
 import android.support.v4.app.ActivityCompat;
 import android.telephony.CellInfo;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.lang.reflect.Array;
@@ -20,34 +28,36 @@ import java.util.List;
 public class MyPhoneReceiver extends BroadcastReceiver {
 
     private String phoneNumber;
+    private static WindowManager windowManager;
+    private static ViewGroup windowLayout;
 
     @Override
     public void onReceive(Context context, Intent intent) {
-//        TelephonyManager tMgr = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
+        /*TelephonyManager tMgr = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
         TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
 
 
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_STATE)
                 != PackageManager.PERMISSION_GRANTED) {
-            /*String getSimOperatorName = tMgr.getLine1Number();
+            String getSimOperatorName = tMgr.getLine1Number();
             String getDeviceId = tMgr.getDeviceId();
 
-            Toast.makeText(context, "getDeviceId: " + getDeviceId,Toast.LENGTH_SHORT).show();*/
+            Toast.makeText(context, "getDeviceId: " + getDeviceId,Toast.LENGTH_SHORT).show();
         }
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
-//            List<CellInfo> mPhoneNumber = tm.getAllCellInfo();
-//            Toast.makeText(context, "getSimOperatorName: " + mPhoneNumber,Toast.LENGTH_SHORT).show();
+            List<CellInfo> mPhoneNumber = tm.getAllCellInfo();
+            Toast.makeText(context, "getSimOperatorName: " + mPhoneNumber,Toast.LENGTH_SHORT).show();
         }
         String getLine1Number = tm.getLine1Number();
-//        String getDeviceId = tMgr.getDeviceId();
+        String getDeviceId = tMgr.getDeviceId();
 
-//        Toast.makeText(context, "getDeviceId: " + getDeviceId,Toast.LENGTH_SHORT).show();
-//        Toast.makeText(context, "getSimOperatorName: " + getLine1Number,Toast.LENGTH_LONG).show();
+        Toast.makeText(context, "getDeviceId: " + getDeviceId,Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, "getSimOperatorName: " + getLine1Number,Toast.LENGTH_LONG).show();*/
         phoneNumber = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
         String extraState = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+        showWindow(context, phoneNumber);
 
-//        Log.d(Constants.TAG, "MyPhoneReciever phoneNumber "+phoneNumber);
         if (MainActivity.updateExternalStorageState() == Constants.MEDIA_MOUNTED) {
             try {
                 SharedPreferences settings = context.getSharedPreferences(
@@ -93,6 +103,40 @@ public class MyPhoneReceiver extends BroadcastReceiver {
                 Log.e(Constants.TAG, "Exception");
                 e.printStackTrace();
             }
+        }
+    }
+
+    private void showWindow(Context context, String phone) {
+        windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
+                PixelFormat.TRANSLUCENT);
+        params.gravity = Gravity.TOP;
+
+        windowLayout = (ViewGroup) layoutInflater.inflate(R.layout.info, null);
+
+        TextView textViewNumber=(TextView) windowLayout.findViewById(R.id.textViewNumber);
+        Button buttonClose=(Button) windowLayout.findViewById(R.id.buttonClose);
+        textViewNumber.setText(phone);
+        buttonClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                closeWindow();
+            }
+        });
+
+        windowManager.addView(windowLayout, params);
+    }
+
+    private void closeWindow() {
+        if (windowLayout !=null){
+            windowManager.removeView(windowLayout);
+            windowLayout =null;
         }
     }
 
